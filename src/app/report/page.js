2678da05e59f404c143ee183f89a2b2b1665767f
MiaNguyen912 <thuydn7@uci.edu>
2024-11-12 01:59:33 -0800

    adjust styling

--- src/app/report/page.js
+++ src/app/report/page.js
@@ -7,13 +7,13 @@ export default function Report() {
     <div className="bg-light-blue relative isolate overflow-clip">
         {/* paralax background decor*/}
         <div className="fixed -z-10">
-            <div aria-hidden="true" className="absolute top-[-30rem] blur-3xl transform-gpu">
+            <div aria-hidden="true" className="absolute top-[-30rem] left-[25vw] blur-3xl transform-gpu">
                 <div
                     style={{clipPath: 'polygon(50% 0%, 65% 8%, 78% 5%, 88% 18%, 98% 35%, 90% 50%, 85% 65%, 70% 75%, 55% 88%, 40% 75%, 28% 85%, 15% 70%, 5% 52%, 12% 35%, 20% 22%, 35% 15%, 45% 5%)'}}
                     className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-pink-500 to-indigo-500 opacity-80"
                 />
             </div>
-            <div aria-hidden="true" className="absolute top-[0rem] blur-3xl transform-gpu">
+            <div aria-hidden="true" className="absolute top-[10rem] left-[10vw] blur-3xl transform-gpu">
                 <div
                     style={{clipPath: 'polygon(8% 82%, 73% 32%, 43% 91%, 15% 55%)',}}
                     className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-pink-500 to-indigo-500 opacity-80"
@@ -27,7 +27,7 @@ export default function Report() {
         </main>
 
         {/* menu bar */}
-        <MenuBar activeButton={"calendar"}/>
+        <MenuBar activeButton={"report"}/>
     </div>
   );
 }
