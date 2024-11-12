ceb1a4ece299e720fd4421a7d89812151a5ee689
MiaNguyen912 <thuydn7@uci.edu>
2024-11-12 00:48:27 -0800

    config global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,32 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        "primary": "#613EEA", // purple
+        "secondary": "#f5f5f5", // off-white
+        "tertiary": '#9DB2CE', // gray
+        "accent": "#EF2A39", // hot-pink
+        'light-blue': "#c1e4f1", // light blue
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        'unicorn': 'linear-gradient(to top right, #c5e5f1, #b794fe, #e1a6f6)',
+        'unicorn-pink-purple': 'linear-gradient(to right, #F1B3EE, #435EED)',
+        'unicorn-purple-purple': 'linear-gradient(to bottom right, #613EEA, #5337C1)',
+      },
+
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+       '.text-shadow-glow-pink': {
+          textShadow: '0 0 20px #e17caa, 0 0 40px #f4d9ea',
+        },
+
+        
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 };
