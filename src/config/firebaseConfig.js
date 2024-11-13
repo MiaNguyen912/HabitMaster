d567faabb241e2c92e153aa7c8422028ecf78d25
MiaNguyen912 <thuydn7@uci.edu>
2024-11-13 15:38:52 -0800

    add comment

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+//  Add SDKs for Firebase products that you want to use (https://firebase.google.com/docs/web/setup#available-libraries)
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
@@ -22,4 +22,4 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { app, db, auth };
+export { app, db, auth, analytics };
