6ec19a3da5cb472a4f7e755f75f792bbdcc48bc6
brianna steier <bsteier@briannas-MacBook-Pro.local>
2024-12-09 11:38:03 -0800

    Implemented sign out and update password.

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -4,37 +4,91 @@ import axios from 'axios';
 import MenuBar from "@/components/menu-bar";
 import GoBackHeader from "@/components/goback-header";
 import { ChevronDownIcon } from '@heroicons/react/16/solid'
-
-
+import {signOut, updatePassword, EmailAuthProvider, reauthenticateWithCredential } from "firebase/auth";
+import { auth } from "@/data/firebaseConfig";
 
 
 
 export default function Profile() {
-    
+    const [formData, setFormData] = useState({
+        currentPassword: "",
+        newPassword: "",
+        confirmPassword: ""
+    });
+
     useEffect(() => {
         async function fetchData() {
-          try {
-           
-          } catch (error) {
-          }
-        }
-       
-        fetchData();
-    }, []);  
+            try {
 
-    function handleSignOut() {
-      alert("sign out");
-        // localStorage.removeItem("currentUser");
-        // window.location.href = "/";
-    } 
+            } catch (error) {
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    const handleSignOut = () => {
+
+        signOut(auth)
+            .then(() => {
+                console.log("User signed out successfully");
+                localStorage.removeItem("currentUser");
+                window.location.href = "/";
+
+            })
+            .catch((error) => {
+                console.error("Error signing out: ", error);
+            });
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value, // Dynamically update the field based on the input's "name"
+        }));
+    };
 
     function handleInfoUpdate(event) {
         event.preventDefault();
-        alert("update info");
 
+        const currentPassword =formData.currentPassword;
+        const newPassword = formData.newPassword;
+        const confirmPassword = formData.confirmPassword;
+
+        if (newPassword !== confirmPassword) {
+            alert("New password and confirmation password do not match.");
+            return;
+        }
+
+        const user = auth.currentUser;
+
+        if (user) {
+            // Reauthenticate the user
+            const credential = EmailAuthProvider.credential(user.email, currentPassword);
+
+            reauthenticateWithCredential(user, credential)
+                .then(() => {
+                    // Update password
+                    return updatePassword(user, newPassword);
+                })
+                .then(() => {
+                    alert("Password updated successfully!");
+                })
+                .catch((error) => {
+                    console.error("Error updating password:", error);
+                    if (error.code === "auth/wrong-password") {
+                        alert("The current password is incorrect.");
+                    } else {
+                        alert("Failed to update password. Please try again.");
+                    }
+                });
+        } else {
+            alert("No user is signed in.");
+        }
     }
-    
-   
+
+
     return (
         <div className="bg-light-blue relative isolate overflow-clip">
             {/* paralax background decor*/}
@@ -55,101 +109,99 @@ export default function Profile() {
 
             {/* header */}
             <GoBackHeader text="Home"/>
-            
+
             {/* main content */}
-            <div className="flex flex-col items-center justify-center bg-gray-800 h-screen overflow-scroll ">        
-              <form className="pt-20 max-sm:pt-40 mb-40" onSubmit={handleInfoUpdate}>
-                <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-                  <div className="col-span-full flex items-center gap-x-8">
-                    <img
-                      alt=""
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      className="size-24 flex-none rounded-lg bg-gray-800 object-cover"
-                    />
-                    <div>
-                      <button type="button" className="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20" >
-                        Change avatar
-                      </button>
-                      <p className="mt-2 text-xs/5 text-gray-400">JPG, GIF or PNG. 1MB max.</p>
+            <div className="flex flex-col items-center justify-center bg-gray-800 h-screen overflow-scroll ">
+                <form className="pt-20 max-sm:pt-40 mb-40" onSubmit={handleInfoUpdate}>
+                    <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
+                        <div className="col-span-full flex items-center gap-x-8">
+                            <img
+                                alt=""
+                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                className="size-24 flex-none rounded-lg bg-gray-800 object-cover"
+                            />
+                            <div>
+                                <button type="button"
+                                        className="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">
+                                    Change avatar
+                                </button>
+                                <p className="mt-2 text-xs/5 text-gray-400">JPG, GIF or PNG. 1MB max.</p>
+                            </div>
+                        </div>
+
+                        <div className="col-span-full">
+                            <h1 className="text-2xl font-bold text-white">Update Password</h1>
+                        </div>
+
+                        <div className="col-span-full">
+                            <label htmlFor="current-password" className="block text-sm/6 font-medium text-white">
+                                Current password
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="current-password"
+                                    name="currentPassword"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                    value={formData.currentPassword}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="col-span-full">
+                            <label htmlFor="new-password" className="block text-sm/6 font-medium text-white">
+                                New password
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="new-password"
+                                    name="newPassword"
+                                    type="password"
+                                    autoComplete="new-password"
+                                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                    value={formData.newPassword}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="col-span-full">
+                            <label htmlFor="confirm-password" className="block text-sm/6 font-medium text-white">
+                                Confirm password
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="confirm-password"
+                                    name="confirmPassword"
+                                    type="password"
+                                    autoComplete="new-password"
+                                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                                    value={formData.confirmPassword}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
                     </div>
-                  </div>
 
-                  <div className="col-span-full">
-                    <label htmlFor="email" className="block text-sm/6 font-medium text-white">
-                      Email address
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                      />
+                    <div className="mt-8 flex gap-4 w-full">
+                        <button type="submit"
+                                className="rounded-md bg-primary w-[50%] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-accent">
+                            Save
+                        </button>
+
+                        <button type="button" onClick={handleSignOut}
+                                className="rounded-md bg-white/10 w-[50%] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">
+                            Sign out
+                        </button>
                     </div>
-                  </div>
-
-                  <div className="col-span-full">
-                    <label htmlFor="current-password" className="block text-sm/6 font-medium text-white">
-                      Current password
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        id="current-password"
-                        name="current_password"
-                        type="password"
-                        autoComplete="current-password"
-                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="col-span-full">
-                    <label htmlFor="new-password" className="block text-sm/6 font-medium text-white">
-                      New password
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        id="new-password"
-                        name="new_password"
-                        type="password"
-                        autoComplete="new-password"
-                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="col-span-full">
-                    <label htmlFor="confirm-password" className="block text-sm/6 font-medium text-white">
-                      Confirm password
-                    </label>
-                    <div className="mt-2">
-                      <input
-                        id="confirm-password"
-                        name="confirm_password"
-                        type="password"
-                        autoComplete="new-password"
-                        className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="mt-8 flex gap-4 w-full">
-                  <button type="submit" className="rounded-md bg-primary w-[50%] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-accent">
-                    Save
-                  </button>
-
-                  <button type="button" onClick={handleSignOut} className="rounded-md bg-white/10 w-[50%] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">
-                    Sign out
-                  </button>  
-                </div>
-              </form>
+                </form>
             </div>
-            
+
 
             {/* menu bar */}
-            <MenuBar activeButton={"report"}/>
+            <MenuBar activeButton={"profile"}/>
         </div>
     );
 }
